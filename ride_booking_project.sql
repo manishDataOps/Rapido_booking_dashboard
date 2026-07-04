b16dd5fb9a9c6a247284b640fe505ac2b0af596d
manish <manish.dataops@gmail.com>
2026-07-04 06:14:31 +0000

    inserted dashboard overview

--- ride_booking_project.sql
+++ ride_booking_project.sql
@@ -1,8 +1,13 @@
+--- rapido booking cancellation analysis
 
-select 
-city, count(booking_id) as total_booking
-from booking_rawdata
-group by city;
-
-
+-- ride booking overview KPI
+SELECT 
+COUNT(booking_id) AS total_booking,
+SUM( CASE WHEN ride_status = 'completed' THEN 1 ELSE 0 END ) AS total_complete_ride,
+SUM( CASE WHEN ride_status = 'cancelled' THEN 1 ELSE 0 END ) AS total_cancelled_rid,
+CAST( SUM( CASE WHEN ride_status = 'cancelled' THEN 1 ELSE 0 END ) * 100.0 / COUNT(booking_id) AS DECIMAL(5,2)) AS cancellation_rate,
+SUM( actual_fare) AS total_complete_ride_amount,
+SUM( CASE WHEN ride_status = 'cancelled' THEN estimated_fare ELSE 0 END ) AS total_revenue_loss,
+SUM( actual_fare) + SUM( CASE WHEN ride_status = 'cancelled' THEN estimated_fare ELSE 0 END ) AS potencial_amount
+FROM booking_rawdata;
 
